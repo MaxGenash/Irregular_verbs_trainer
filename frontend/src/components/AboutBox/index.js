30fecc3add80eb19581cc8fe54973bbd10b268ae
MaxGenash <123maks189@mail.ru>
2017-04-04 13:26:36 +0300

    Made markup of about page

--- frontend/src/components/AboutBox/index.js
+++ frontend/src/components/AboutBox/index.js
@@ -5,7 +5,44 @@ export default class AboutBox extends Component {
     render() {
         return (
             <div className=''>
-                <h1 className=''> AboutBox </h1>
+                <h1 className='text-center'> Про програму </h1>
+                <p className="text-center">
+                    <i>
+                        Тепер вчити неправильні дієслова англійської мови стало легко!
+                    </i>
+                </p>
+                <div className="row">
+                    <div className="col-xs-12 col-md-4">
+                        <div className="thumbnail text-center">
+                            <h1>
+                                <i className="fa fa-book fa-4x" />
+                            </h1>
+                            <div className="caption">
+                                <h4> Зручний перегляд теорії </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-md-4">
+                        <div className="thumbnail text-center">
+                            <h1>
+                                <i className="fa fa-list-alt fa-4x" />
+                            </h1>
+                            <div className="caption">
+                                <h4> Контроль знань через тести </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-md-4">
+                        <div className="thumbnail text-center">
+                            <h1>
+                                <i className="fa fa-line-chart fa-4x" />
+                            </h1>
+                            <div className="caption">
+                                <h4> Контроль прогресу вивчення </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
